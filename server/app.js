4f0d328aaf7e5e0822b8ba56cd11998fa88689c5
lius <SQJD381@163.com>
2019-02-13 17:53:20 +0800

    引入mongoose 增加商品路由

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var goodsRouter = require('./routes/goods');
 var ejs = require('ejs');
 
 var app = express();
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/goods', goodsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
