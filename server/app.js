2362de3060f12d4a6da13f98990d8ea8fb24276b
lius <SQJD381@163.com>
2019-03-01 11:31:27 +0800

    登录拦截

--- server/app.js
+++ server/app.js
@@ -52,4 +52,21 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.use(function (req,res,next) {
+  if(req.cookies.userId){
+    next();
+  }else{
+    console.log("url:"+req.originalUrl);
+    if(req.originalUrl=='/users/login' || req.originalUrl=='/users/logout' || req.originalUrl.indexOf('/goods/list')>-1){
+      next();
+    }else{
+      res.json({
+        status:'10001',
+        msg:'当前未登录',
+        result:''
+      });
+    }
+  }
+});
+
 module.exports = app;
