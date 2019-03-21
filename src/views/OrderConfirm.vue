8315c8c756f898000b9383095ba656eab863761d
lius <SQJD381@163.com>
2019-03-21 17:49:26 +0800

    订单确认页面的初始化

---
+++ src/views/OrderConfirm.vue
@@ -0,0 +1,23 @@
+/**
+*Created by Administrator on 2019/3/21
+*/
+<template>
+    <div>
+      {{msg}}
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorld',
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
